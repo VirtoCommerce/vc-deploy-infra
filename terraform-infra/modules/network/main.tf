88a8fbd528da8da05054be0560d94a54ff98895c
Konstantin Kisilevskiy <xlonelysoul94@gmail.com>
2021-01-15 14:18:13 +0200

    VP-6643: Activate Security group

--- terraform-infra/modules/network/main.tf
+++ terraform-infra/modules/network/main.tf
@@ -16,10 +16,16 @@ resource "azurerm_subnet" "subnet2" {
   virtual_network_name = azurerm_virtual_network.net1.name
   resource_group_name  = var.resource_group_name
   address_prefixes     = ["10.2.0.0/16"]
+
 }
 resource "azurerm_subnet" "subnet3" {
   name                 = "akssubnet3"
   virtual_network_name = azurerm_virtual_network.net1.name
   resource_group_name  = var.resource_group_name
   address_prefixes     = ["10.3.0.0/16"]
+}
+
+resource "azurerm_subnet_network_security_group_association" "subnet2asso" {
+  subnet_id                 = azurerm_subnet.subnet2.id
+  network_security_group_id = "/subscriptions/a6522b3e-cc34-41c8-bf38-a5ff3080186a/resourceGroups/mc_cloud-platform_vc-master_eastus/providers/Microsoft.Network/networkSecurityGroups/aks-agentpool-34239724-nsg"
 }
