69dce13b622a4b8a5bf264d2010d18909b5188c2
Konstantin Kisilevskiy <xlonelysoul94@gmail.com>
2021-04-09 10:02:51 +0200

    VCI-38: SaaS limits configuration

--- terraform-infra/modules/k8s/main.tf
+++ terraform-infra/modules/k8s/main.tf
@@ -50,13 +50,30 @@ resource "azurerm_kubernetes_cluster" "k8s" {
 }
 resource "azurerm_kubernetes_cluster_node_pool" "app" {
   name                  = "applications"
-  enable_auto_scaling   = "true"
-  max_count             = "4"
-  min_count             = "1"
+  enable_auto_scaling   = "false"
+  max_count             = "0"
+  min_count             = "0"
   kubernetes_cluster_id = azurerm_kubernetes_cluster.k8s.id
   vm_size               = "Standard_E2ds_v4"
   availability_zones    = ["1", "2", "3"]
-  node_count            = "1"
+  node_count            = "4"
+  vnet_subnet_id        = var.subnet_default
+  max_pods              = var.max_pods
+
+  tags = {
+    Environment = "Apps"
+  }
+}
+
+resource "azurerm_kubernetes_cluster_node_pool" "apps" {
+  name                  = "apool"
+  enable_auto_scaling   = "false"
+  max_count             = "0"
+  min_count             = "0"
+  kubernetes_cluster_id = azurerm_kubernetes_cluster.k8s.id
+  vm_size               = "Standard_D2ds_v4"
+  availability_zones    = ["1", "2", "3"]
+  node_count            = "2"
   vnet_subnet_id        = var.subnet_default
   max_pods              = var.max_pods
 
