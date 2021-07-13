68de503a4d8cef8b7130f4d6515608f125de9d73
Konstantin Kisilevskiy <xlonelysoul94@gmail.com>
2021-07-13 14:22:41 +0200

    VCI-70: Enable uptime SLA

--- terraform-infra/modules/k8s/main.tf
+++ terraform-infra/modules/k8s/main.tf
@@ -3,6 +3,7 @@ resource "azurerm_kubernetes_cluster" "k8s" {
   resource_group_name = var.resource_group_name
   location            = var.location
   dns_prefix          = var.dns_prefix
+  sku_tier            = "Paid"
 
   linux_profile {
     admin_username = "ubuntu"
